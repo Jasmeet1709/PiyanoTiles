0f7ce4e8901f4724482188c08367d957c4f431b6
Jasmeet1709 <jsmtbagga45@gmail.com>
2018-08-15 17:30:57 +0530

    added splash screen activity

---
+++ app/src/main/java/com/example/jasmeetsingh/piyanotiles/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.example.jasmeetsingh.piyanotiles;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spash_screen);
+
+
+        int TIME = 2000; //2000 ms 2 Seconds)
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME);
+
+    }
+}
